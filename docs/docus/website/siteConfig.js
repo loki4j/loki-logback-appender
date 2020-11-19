d737729980d579cac03da334f08de1c31d6ec262
nehaev <nehaev@gmail.com>
2020-11-19 23:30:11 +0300

    GA Id added

--- docs/docus/website/siteConfig.js
+++ docs/docus/website/siteConfig.js
@@ -94,6 +94,7 @@ const siteConfig = {
   twitterImage: 'img/undraw_tweetstorm.svg',
 
   twitterUsername: 'arnehaev',
+  gaTrackingId: 'G-0Z4FEXM003',
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
