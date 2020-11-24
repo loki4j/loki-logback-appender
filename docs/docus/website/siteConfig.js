28ae7b2d289fe123e90a436fa8dccb1dbed7f899
nehaev <nehaev@gmail.com>
2020-11-25 00:52:30 +0300

    Bump v0.3.2

--- docs/docus/website/siteConfig.js
+++ docs/docus/website/siteConfig.js
@@ -27,8 +27,8 @@ const siteConfig = {
   title: 'Loki4j Logback', // Title for your website.
   tagline: 'Pure Java Logback appender for Grafana Loki',
   url: 'https://loki4j.github.io', // Your website URL
-  //baseUrl: '/',
-  baseUrl: '/loki-logback-appender/', // Base URL for your project */
+  baseUrl: '/',
+  //baseUrl: '/loki-logback-appender/', // Base URL for your project */
 
   // Used for publishing and more
   projectName: 'loki-logback-appender',
@@ -112,7 +112,7 @@ const siteConfig = {
   repoUrl: ghUrl,
   startDoc: defaultDoc,
 
-  artifactVersion: '0.3.1',
+  artifactVersion: '0.3.2',
 };
 
 module.exports = siteConfig;
