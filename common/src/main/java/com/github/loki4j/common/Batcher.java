6935f1884206df7c8ce0a20ce5df97e2373e68b3
nehaev <nehaev@gmail.com>
2021-02-23 02:43:49 +0300

    Binary batcher sketch impl

--- common/src/main/java/com/github/loki4j/common/Batcher.java
+++ common/src/main/java/com/github/loki4j/common/Batcher.java
@@ -1,24 +1,67 @@
 package com.github.loki4j.common;
 
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+
 public final class Batcher {
 
+    private final int maxSizeBytes;
     private final long maxTimeoutMs;
-    private final LogRecord[] items;
-    private int index = 0;
+    private final Integer[] itemOffsets;
+    private final ByteBuffer buf;
 
-    public Batcher(int maxItems, long maxTimeoutMs) {
+    private int index = 0;
+    private int sizeBytes = 0;
+    private HashMap<byte[], Integer> streams = new HashMap<>();
+
+
+    public Batcher(int maxItems, int maxSizeBytes, long maxTimeoutMs, ByteBuffer buf) {
+        this.maxSizeBytes = maxSizeBytes;
         this.maxTimeoutMs = maxTimeoutMs;
-        this.items = new LogRecord[maxItems];
+        this.itemOffsets = new Integer[maxItems];
+        this.buf = buf;
+    }
+
+    private long estimateSizeBytes(byte[] stream, int msgLen, boolean dryRun) {
+        // +4 - message field id in pb
+        long size = msgLen + 4;
+        if (!streams.containsKey(stream)) {
+            // +24 - constant stream overhead in json
+            size += stream.length + 24;
+            if (!dryRun) {
+                streams.put(stream, buf.position());
+                buf.putInt(stream.length);
+                buf.put(stream);
+            }
+        }
+        return size;
     }
 
     private void cutBatchAndReset(LogRecordBatch destination, BatchCondition condition) {
-        destination.initFrom(items, index, condition);
+        destination.initFrom(itemOffsets, index, buf.asReadOnlyBuffer(), condition, sizeBytes);
         index = 0;
+        sizeBytes = 16; // 16 - constant starter size in json (in pb it's lesser)
+        streams.clear();
     }
 
-    public void add(LogRecord input, LogRecordBatch destination) {
-        items[index] = input;
-        if (++index == items.length)
+    public boolean checkSize(byte[] stream, int msgLen, LogRecordBatch destination) {
+        var recordSizeBytes = estimateSizeBytes(stream, msgLen, true);
+        if (recordSizeBytes > maxSizeBytes)
+            return false;
+
+        if (sizeBytes + recordSizeBytes > maxSizeBytes)
+            cutBatchAndReset(destination, BatchCondition.MAX_BYTES);
+        return true;
+    }
+
+    public void add(byte[] stream, long msgTs, byte[] msg, LogRecordBatch destination) {
+        itemOffsets[index] = buf.position();
+        sizeBytes += estimateSizeBytes(stream, msg.length, false);
+        buf.putInt(streams.get(stream));
+        buf.putLong(msgTs);
+        buf.putInt(msg.length);
+        buf.put(msg); // TODO: buffer overflow!
+        if (++index == itemOffsets.length)
             cutBatchAndReset(destination, BatchCondition.MAX_ITEMS);
     }
 
@@ -29,7 +72,7 @@ public final class Batcher {
     }
 
     public int getCapacity() {
-        return items.length;
+        return itemOffsets.length;
     }
 
 }
--- common/src/main/java/com/github/loki4j/common/LogRecordBatch.java
+++ common/src/main/java/com/github/loki4j/common/LogRecordBatch.java
@@ -1,54 +1,73 @@
 package com.github.loki4j.common;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class LogRecordBatch {
 
+    private static final int TS_OFFSET = 4;
+
+    private final boolean sortByStream;
+
+    private final boolean sortByTime;
+
     private long batchId;
-
-    private LogRecord[] records;
-
+    private Integer[] itemOffsets;
     private int len;
-
+    private ByteBuffer buf;
     private BatchCondition condition;
+    private int estimatedSizeBytes;
 
-    public LogRecordBatch(int capacity) {
-        records = new LogRecord[capacity];
-        clear();
-    }
+    public LogRecordBatch(boolean sortByStream, boolean sortByTime) {
+        this.sortByStream = sortByStream;
+        this.sortByTime = sortByTime;
 
-    public LogRecordBatch(LogRecord[] source) {
-        records = new LogRecord[source.length];
-        clear();
-        initFrom(source, source.length, BatchCondition.UNKNOWN);
-    }
-
-    public void initFrom(LogRecord[] source, int len, BatchCondition condition) {
-        if (len > records.length)
-            throw new RuntimeException(String.format(
-                "Source length %s exceeds available capacity %s", len, records.length));
-        batchId = System.nanoTime();
-        System.arraycopy(source, 0, records, 0, len);
-        this.len = len;
-        this.condition = condition;
-    }
-
-    public void clear() {
-        batchId = 0;
-        len = 0;
-        Arrays.setAll(records, i -> null);
         condition = BatchCondition.UNKNOWN;
+        // all other fields are initialized with their default values
     }
 
-    public void sort(Comparator<LogRecord> comp) {
-        Arrays.sort(records, 0, len, comp);
+    void initFrom(Integer[] itemOffsets, int len, ByteBuffer buf, BatchCondition condition, int estimatedSizeBytes) {
+        this.itemOffsets = itemOffsets;
+        this.len = len;
+        this.buf = buf;
+        this.condition = condition;
+        this.estimatedSizeBytes = estimatedSizeBytes;
+
+        batchId = System.nanoTime();
+        sort();
     }
 
-    public LogRecord get(int index) {
+    private void sort() {
+        var comp = generateComparator();
+        if (comp != null)
+            Arrays.sort(itemOffsets, 0, len, comp);
+    }
+
+    private Comparator<Integer> generateComparator() {
+        if (sortByStream) {
+            Comparator<Integer> byStream = (i1, i2) -> 
+                Integer.compare(buf.getInt(i1), buf.getInt(i2));
+            if (sortByTime) {
+                Comparator<Integer> byTime = (i1, i2) -> 
+                    Long.compare(buf.getLong(i1 + TS_OFFSET), buf.getLong(i2 + TS_OFFSET));
+                return byStream.thenComparing(byTime);
+            } else {
+                return byStream;
+            }
+        } else if (sortByTime) {
+            Comparator<Integer> byTime = (i1, i2) -> 
+                Long.compare(buf.getLong(i1 + TS_OFFSET), buf.getLong(i2 + TS_OFFSET));
+            return byTime;
+        } else {
+            return null;
+        }
+     }
+
+    public int getStreamId(int index) {
         if (index < 0 || index >= len)
             throw new IndexOutOfBoundsException("LogRecordBatch: Index out of range: " + index);
-        return records[index];
+        return buf.getInt(itemOffsets[index]);
     }
 
     public long batchId() {
@@ -64,21 +83,25 @@ public class LogRecordBatch {
     }
 
     public int capacity() {
-        return records.length;
+        return itemOffsets.length;
     }
 
     public BatchCondition getCondition() {
         return condition;
     }
 
-    public LogRecord[] toArray() {
-        return Arrays.copyOf(records, len);
+    public int getEstimatedSizeBytes() {
+        return estimatedSizeBytes;
     }
+
+    /*public LogRecord[] toArray() {
+        return Arrays.copyOf(records, len);
+    }*/
 
     @Override
     public String toString() {
         return String.format(
-            "#%x (%s, %s records)",
-            batchId, condition, len);
+            "#%x (%s, %s records, est. size %,d bytes)",
+            batchId, condition, len, estimatedSizeBytes);
     }
 }
