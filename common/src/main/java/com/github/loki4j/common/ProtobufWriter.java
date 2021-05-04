5ac1451faa9cb6a48c1db068e2ecc01926f7b8df
nehaev <nehaev@gmail.com>
2021-05-05 00:13:50 +0300

    Compressed PB buffer size is x1.5 of the limit for uncompressed PB

--- common/src/main/java/com/github/loki4j/common/ProtobufWriter.java
+++ common/src/main/java/com/github/loki4j/common/ProtobufWriter.java
@@ -22,7 +22,9 @@ public final class ProtobufWriter implements Writer {
 
     public ProtobufWriter(int capacity, ByteBufferFactory bbFactory) {
         this.uncompressed = bbFactory.allocate(capacity);
-        this.compressed = bbFactory.allocate(capacity);
+        // allocating x1.5 for compressed buffer, as compressed size
+        // may be larger than uncompressed
+        this.compressed = bbFactory.allocate(capacity + capacity / 2);
         this.request = PushRequest.newBuilder();
     }
 
