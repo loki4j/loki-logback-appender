9d1945e3500ba73d43b6a055914c75906a1add55
Anton Nekhaev <nehaev@gmail.com>
2024-01-30 02:01:18 +0200

    Proper target length config for logger_name

--- loki-logback-appender/src/main/java/com/github/loki4j/logback/json/LoggerNameJsonProvider.java
+++ loki-logback-appender/src/main/java/com/github/loki4j/logback/json/LoggerNameJsonProvider.java
@@ -1,6 +1,8 @@
 package com.github.loki4j.logback.json;
 
 import ch.qos.logback.classic.pattern.Abbreviator;
+import ch.qos.logback.classic.pattern.ClassNameOnlyAbbreviator;
+import ch.qos.logback.classic.pattern.TargetLengthBasedClassNameAbbreviator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
 public class LoggerNameJsonProvider extends AbstractFieldJsonProvider {
@@ -8,12 +10,34 @@ public class LoggerNameJsonProvider extends AbstractFieldJsonProvider {
     public static final String FIELD_LOGGER_NAME = "logger_name";
 
     /**
-     * Abbreviator that will shorten the logger classname
+     * The desired target length:
+     * {@code -1} to disable abbreviation,
+     * {@code 0} to print class name only,
+     * {@code >0} to abbreviate to the target length.
      */
-    private Abbreviator abbreviator = new NoOpAbbreviator();
+    private int targetLength = -1;
+
+    private Abbreviator abbreviator;
 
     public LoggerNameJsonProvider() {
         setFieldName(FIELD_LOGGER_NAME);
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        if (targetLength > 0)
+            abbreviator = new TargetLengthBasedClassNameAbbreviator(targetLength);
+        else if (targetLength == 0)
+            abbreviator = new ClassNameOnlyAbbreviator();
+        else
+            abbreviator = new NoOpAbbreviator();
+    }
+
+    @Override
+    public void stop() {
+        abbreviator = null;
+        super.stop();
     }
 
     @Override
@@ -21,8 +45,8 @@ public class LoggerNameJsonProvider extends AbstractFieldJsonProvider {
         writer.writeStringField(getFieldName(), abbreviator.abbreviate(event.getLoggerName()));
     }
 
-    public void setAbbreviator(Abbreviator abbreviator) {
-        this.abbreviator = abbreviator;
+    public void setTargetLength(int targetLength) {
+        this.targetLength = targetLength;
     }
 
     public static class NoOpAbbreviator implements Abbreviator {
