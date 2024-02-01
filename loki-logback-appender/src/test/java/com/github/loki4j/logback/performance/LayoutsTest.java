bd2fe8574d2cb525a15bd975a131a5c9b4be81c7
Anton Nehaev <nehaev@users.noreply.github.com>
2024-02-01 23:51:35 +0200

    Add performance test for JsonLayer (#210)

---
+++ loki-logback-appender/src/test/java/com/github/loki4j/logback/performance/LayoutsTest.java
@@ -0,0 +1,59 @@
+package com.github.loki4j.logback.performance;
+
+import static com.github.loki4j.logback.Generators.generateEvents;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import com.github.loki4j.logback.JsonLayout;
+import com.github.loki4j.logback.Generators.InfiniteEventIterator;
+import com.github.loki4j.testkit.benchmark.Benchmarker;
+import com.github.loki4j.testkit.benchmark.Benchmarker.Benchmark;
+import com.github.loki4j.testkit.categories.PerformanceTests;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Layout;
+
+import net.logstash.logback.layout.LogstashLayout;
+
+public class LayoutsTest {
+
+    private static Layout<ILoggingEvent> initLayout(Layout<ILoggingEvent> l) {
+        l.setContext(new LoggerContext());
+        l.start();
+        return l;
+    }
+    
+    @Test
+    @Category({PerformanceTests.class})
+    public void layoutPerformance() throws Exception {
+        var statsLayouts = Benchmarker.run(new Benchmarker.Config<ILoggingEvent>() {{
+            this.runs = 100;
+            this.parFactor = 1;
+            this.generator = () -> InfiniteEventIterator.from(generateEvents(10_000, 10)).limited(100_000);
+            this.benchmarks = Arrays.asList(
+                Benchmark.of("patternLayout",
+                    () -> initLayout(new PatternLayout()),
+                    (layout, e) -> layout.doLayout(e),
+                    layout -> {},
+                    layout -> layout.stop()),
+                Benchmark.of("jsonLayout",
+                    () -> initLayout(new JsonLayout()),
+                    (layout, e) -> layout.doLayout(e),
+                    layout -> {},
+                    layout -> layout.stop()),
+                Benchmark.of("logstashLayout",
+                    () -> initLayout(new LogstashLayout()),
+                    (layout, e) -> layout.doLayout(e),
+                    layout -> {},
+                    layout -> layout.stop())
+            );
+        }});
+        statsLayouts.forEach(System.out::println);
+    }
+
+}
