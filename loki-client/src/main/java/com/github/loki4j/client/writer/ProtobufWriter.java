8586b262ae0c44802859c9389d08f7ca8d496726
Anton Nekhaev <nehaev@gmail.com>
2024-05-15 00:50:49 +0300

    Switched to new proto version

--- loki-client/src/main/java/com/github/loki4j/client/writer/ProtobufWriter.java
+++ loki-client/src/main/java/com/github/loki4j/client/writer/ProtobufWriter.java
@@ -7,9 +7,9 @@ import com.github.loki4j.client.batch.LogRecord;
 import com.github.loki4j.client.batch.LogRecordBatch;
 import com.github.loki4j.client.util.ByteBufferFactory;
 import com.github.loki4j.pkg.google.protobuf.Timestamp;
-import com.github.loki4j.pkg.loki.protobuf.Logproto.EntryAdapter;
-import com.github.loki4j.pkg.loki.protobuf.Logproto.PushRequest;
-import com.github.loki4j.pkg.loki.protobuf.Logproto.StreamAdapter;
+import com.github.loki4j.pkg.loki.protobuf.Push.EntryAdapter;
+import com.github.loki4j.pkg.loki.protobuf.Push.PushRequest;
+import com.github.loki4j.pkg.loki.protobuf.Push.StreamAdapter;
 import com.google.protobuf.CodedOutputStream;
 
 import org.xerial.snappy.Snappy;
