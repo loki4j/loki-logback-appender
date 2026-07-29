9ef384f9645eea8409b84d2a23d0f3b332f55733
Anton Nekhaev <nehaev@gmail.com>
2026-07-30 02:39:42 +0300

    Add snippet copy events counter

--- docs/docus/website/siteConfig.js
+++ docs/docus/website/siteConfig.js
@@ -83,7 +83,11 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  // Local scripts need the baseUrl prefix: Docusaurus uses these paths as-is.
+  scripts: [
+    'https://buttons.github.io/buttons.js',
+    '/loki-logback-appender/js/ga-copy-events.js',
+  ],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
